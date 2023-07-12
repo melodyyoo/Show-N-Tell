54a241dc9133880e9dd9de900a97a3453b380f71
Melody Yoo <melodyyoo72@gmail.com>
2023-07-12 01:23:42 -0700

    feat: add users and shows seed data

--- backend/db/seeders/20230511194002-demo-user.js
+++ backend/db/seeders/20230511194002-demo-user.js
@@ -26,36 +26,36 @@ module.exports = {
        email: 'demo@user.io',
        username: 'Demo-lition',
        hashedPassword: bcrypt.hashSync('password'),
-       firstName: 'Keanu',
+       firstName: 'Peter',
        lastName: 'Reeves'
      },
      {
        email: 'user1@user.io',
        username: 'FakeUser1',
-       hashedPassword: bcrypt.hashSync('password1'),
-       firstName: 'Halle',
+       hashedPassword: bcrypt.hashSync('password'),
+       firstName: 'Kim',
        lastName: 'Bailey'
      },
      {
        email: 'user2@user.io',
        username: 'FakeUser2',
-       hashedPassword: bcrypt.hashSync('password2'),
-       firstName: 'Jihyo',
+       hashedPassword: bcrypt.hashSync('password'),
+       firstName: 'Sandra',
        lastName: 'Park'
      },
      {
       email: 'user3@user.io',
       username: 'FakeUser3',
-      hashedPassword: bcrypt.hashSync('password3'),
+      hashedPassword: bcrypt.hashSync('password'),
       firstName: 'Harry',
-      lastName: 'Styles'
+      lastName: 'Swift'
     },
     {
       email: 'user4@user.io',
       username: 'FakeUser4',
-      hashedPassword: bcrypt.hashSync('password4'),
-      firstName: 'Nicki',
-      lastName: 'Minaj'
+      hashedPassword: bcrypt.hashSync('password'),
+      firstName: 'Sam',
+      lastName: 'Bates'
     }
     ], {});
   },
---
+++ backend/db/seeders/20230712060140-Comments.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   options.tableName = 'Comments';
+   await queryInterface.bulkInsert(options, [
+    {
+      
+    }
+   ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
