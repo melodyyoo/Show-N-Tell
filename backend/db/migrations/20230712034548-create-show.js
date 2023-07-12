85a7c32377d16f3c06d2fa8eb6f05fb1ae8a4a3b
Melody Yoo <melodyyoo72@gmail.com>
2023-07-12 12:19:11 -0700

    fix: nest tablename inside table for addContraint

--- backend/db/migrations/20230712034548-create-show.js
+++ backend/db/migrations/20230712034548-create-show.js
@@ -1,78 +1,83 @@
-'use strict';
-
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
-if(process.env.NODE_ENV === 'production'){
-  options.schema = process.env.SCHEMA
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Shows', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      "Shows",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        image: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        banner: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        genre: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        year: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        director: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        synopsis: {
+          type: Sequelize.STRING(600),
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: { model: "Users" },
+          onDelete: "cascade",
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      name: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      image: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      banner: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      genre: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      year: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      director: {
-        type: Sequelize.STRING,
-        allowNull:false
-      },
-      synopsis: {
-        type: Sequelize.STRING(600),
-        allowNull:false
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        references: {model: "Users"},
-        onDelete:"cascade"
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, options);
+      options
+    );
 
     // options.tableName = "Shows"
     await queryInterface.addConstraint("Shows", {
-      fields: ['name', 'director'],
+      fields: ["name", "director"],
       type: "unique",
       name: "unique-director-show",
       references: {
-        tableName: "Shows",
-        schema: process.env.SCHEMA
-      }
-    })
+        table: {
+          tableName: "Shows",
+          schema: process.env.SCHEMA,
+        },
+      },
+    });
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Shows"
+    options.tableName = "Shows";
     await queryInterface.dropTable(options);
-  }
+  },
 };
