13f57fe68a3e4b9a3bfe0cb3779af10a53c849b0
Melody Yoo <melodyyoo72@gmail.com>
2023-07-14 01:35:31 -0700

    feat: add checkUser helper functoin to SingleShow component

--- frontend/src/components/Shows/SingleShow/index.jsx
+++ frontend/src/components/Shows/SingleShow/index.jsx
@@ -3,17 +3,31 @@ import { useDispatch, useSelector } from "react-redux";
 import { thunkGetShowAndReview } from "../../../store/shows";
 import { useParams } from "react-router-dom";
 import "./SingleShow.css";
+import AllReviews from "../../Reviews/AllReviews";
+import PostReview from "../../PostReview";
 
 export default function SingleShow() {
   const dispatch = useDispatch();
   const { showId } = useParams();
   const show = useSelector((state) => state.shows.show);
+  const sessionUser = useSelector(state=> state.session.user);
+
 
   useEffect(() => {
     dispatch(thunkGetShowAndReview(showId));
   }, [dispatch, showId]);
 
   if (!show) return null;
+
+  const checkUser = () =>{
+    if(sessionUser?.id === show.Show?.User?.id){
+      return <button className="review-button">Edit or Delete Show</button>
+    }else if(!sessionUser){
+      return <button className="review-button">Sign in to log, rate, or review</button>
+    }else{
+      return <PostReview/>
+    }
+  }
 
   return (
     <div>
@@ -29,8 +43,9 @@ export default function SingleShow() {
             <p className="posted-by">Posted by {show.Show?.User?.username}</p>
             <p className="show-synopsis">{show.Show?.synopsis}</p>
         </div>
-        <div>{/* <PostReview/> */}</div>
+        {checkUser()}
       </div>
+      {<AllReviews show={show}/>}
     </div>
   );
 }
