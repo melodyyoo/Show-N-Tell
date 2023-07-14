04ec2a7c9fe8a660bb856e45e0e2d273ba5a343c
Melody Yoo <melodyyoo72@gmail.com>
2023-07-13 22:05:16 -0700

    feat: redirect user to single show when clicking on show poster

--- frontend/src/components/Shows/ShowPoster/index.jsx
+++ frontend/src/components/Shows/ShowPoster/index.jsx
@@ -1,20 +1,21 @@
+import { useHistory } from "react-router-dom";
 import "./ShowPoster.css";
 
 export default function ShowPoster({ show }) {
+  const history = useHistory()
   return (
-    <div className="poster-wrapper">
+    <div className="poster-wrapper" onClick={()=>history.push(`/shows/${show.id}`)}>
       <img className="poster" alt="poster" src={show?.image}></img>
       <div className="poster-stats">
         <div style={{display: "flex"}}>
-          <i class="fa-solid fa-heart" style={{ color: "#EF8832" }}/>
+          <i className="fa-solid fa-heart" style={{ color: "#EF8832" }}/>
           <p style={{margin:0, color:"gray"}}>{show?.showLikes}</p>
         </div>
         <div style={{display: "flex"}}>
-          <i class="fa-solid fa-comment" style={{ color: "#66B9EF" }}/>
+          <i className="fa-solid fa-comment" style={{ color: "#66B9EF" }}/>
           <p style={{margin: 0, color:"gray"}}>{show?.reviewsCount}</p>
         </div>
         <div style={{display: "flex"}}>
-
         </div>
       </div>
     </div>
