47452c5080bc332e257dedad3d5cda9cac7dc9a7
Melody Yoo <melodyyoo72@gmail.com>
2023-07-14 01:34:55 -0700

    feat: add SingleReview component

--- frontend/src/components/Reviews/SingleReview/index.jsx
+++ frontend/src/components/Reviews/SingleReview/index.jsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { thunkGetUser } from "../../../store/user";
+import "./SingleReview.css";
+import starIcons from "../../../hooks/starIcons";
 
-export default function SingleReview(){
-    return(
-        <div>
-            
-        </div>
-    )
+export default function SingleReview({ review }) {
+  const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(thunkGetUser(review.userId));
+  }, [dispatch, review.userId]);
+
+  return (
+    <div className="single-review">
+      <div style={{ display: "flex" }}>
+        <p className="review-user">Review by {user.username}</p>
+        <div style={{ margin: "11px 0 0 10px" }}>{starIcons(review)}</div>
+      </div>
+      <p>{review.body}</p>
+      <div className="review-likes">
+        <i className="fa-solid fa-heart" style={{ color: "gray", display: "flex", alignItems: "center" }}></i>
+        <p style={{ fontSize: "13px", color: "gray", marginLeft: "5px" }}>{review.likes} likes</p>
+      </div>
+    </div>
+  );
 }
