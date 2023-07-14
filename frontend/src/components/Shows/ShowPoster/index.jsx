3561392994928a2a23681b029b53e0662482e8fd
Melody Yoo <melodyyoo72@gmail.com>
2023-07-13 18:28:46 -0700

    style: change show poster CSS

--- frontend/src/components/Shows/ShowPoster/index.jsx
+++ frontend/src/components/Shows/ShowPoster/index.jsx
@@ -1,18 +1,20 @@
 import "./ShowPoster.css";
 
 export default function ShowPoster({ show }) {
-  console.log("SHOW: ", show);
   return (
     <div className="poster-wrapper">
       <img className="poster" alt="poster" src={show?.image}></img>
       <div className="poster-stats">
         <div style={{display: "flex"}}>
           <i class="fa-solid fa-heart" style={{ color: "#EF8832" }}/>
-          <p style={{margin:0}}>{show?.showLikes}</p>
+          <p style={{margin:0, color:"gray"}}>{show?.showLikes}</p>
         </div>
         <div style={{display: "flex"}}>
           <i class="fa-solid fa-comment" style={{ color: "#66B9EF" }}/>
-          <p style={{margin: 0}}>{show?.reviewsCount}</p>
+          <p style={{margin: 0, color:"gray"}}>{show?.reviewsCount}</p>
+        </div>
+        <div style={{display: "flex"}}>
+
         </div>
       </div>
     </div>
