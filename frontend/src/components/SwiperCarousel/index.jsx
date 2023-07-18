a8b1d9295bcc93b28d8636b8cc09e7e2bbf8b6e3
Melody Yoo <melodyyoo72@gmail.com>
2023-07-18 16:53:29 -0700

    feat: add carousel to /shows page

---
+++ frontend/src/components/SwiperCarousel/index.jsx
@@ -0,0 +1,41 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import './Swiper.css';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import ShowPoster from '../Shows/ShowPoster';
+
+export default function SwiperCarousel({shows}) {
+  console.log("SHOWS: ", shows)
+  return (
+    <>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+      {shows.map((show, i)=> <SwiperSlide>
+          <ShowPoster show={show} key={i}/>
+        </SwiperSlide>)}
+
+      </Swiper>
+    </>
+  );
+}
