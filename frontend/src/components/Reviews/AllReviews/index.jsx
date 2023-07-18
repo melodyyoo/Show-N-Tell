df8103cb501fd81acc7fb9f88d41829247127be0
Melody Yoo <melodyyoo72@gmail.com>
2023-07-17 22:59:13 -0700

    feat: sort reviews from newest to oldest

--- frontend/src/components/Reviews/AllReviews/index.jsx
+++ frontend/src/components/Reviews/AllReviews/index.jsx
@@ -3,10 +3,15 @@ import "./AllReviews.css"
 
 export default function AllReviews({show}){
     if(!show || !show.Reviews)return null;
+
+    const sortedReviews = show?.Reviews.sort(
+        (a,b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
+    
     return(
         <div className="all-reviews">
             <p className="pop-reviews-title" style={{width:'fit-content'}}>REVIEWS</p>
-            {show.Reviews.map(review=>{
+            {sortedReviews.map(review=>{
                 return <SingleReview key={review.id} review={review}/>
             })}
         </div>
