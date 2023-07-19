7eade2feb16b734a699ab269127d9b2b681ceca7
Melody Yoo <melodyyoo72@gmail.com>
2023-07-18 23:24:21 -0700

    feat: add LandingPage component

--- frontend/src/components/LandingPage/index.jsx
+++ frontend/src/components/LandingPage/index.jsx
@@ -1,6 +1,12 @@
+import "./LandingPage.css"
 
-export default function LandingPage(){
-    return(
-        <h1>This da landing page</h1>
-    )
+export default function LandingPage() {
+  return (
+    <div className="landing-page-wrapper">
+      <video autoPlay muted loop src="show-n-tell-movie.mp4"></video>
+      <img className="landing-page-tv" alt="landing-page-tv" src="/landing-page-tv.svg"/>
+      <h1 className="landing-page-text" style={{marginTop: "80px"}}>Like and review shows you've watched.</h1>
+      <h1 className="landing-page-text">Tell your friends what's good.</h1>
+    </div>
+  );
 }
