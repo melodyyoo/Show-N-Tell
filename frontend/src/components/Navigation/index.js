1fcfe473e4d1aa87ec6b113adc4d3bb15863e48f
Melody Yoo <melodyyoo72@gmail.com>
2023-07-18 23:32:30 -0700

    feat: conditionally render nav bar behind banner images

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -7,28 +7,29 @@ import logo from "./logo.svg";
 import OpenModalButton from "../OpenModalButton";
 import AddShowModal from "../Shows/AddShowModal";
 import LoginFormModal from "../LoginFormModal";
+import { useLocation } from "react-router-dom";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
+  const {pathname} = useLocation();
 
   const modalComponent = sessionUser ? <AddShowModal/> : <LoginFormModal/>
   const title = sessionUser ? "DON'T SEE A SHOW? ADD ONE NOW!" : "SIGN IN";
 
+  const splitPath = pathname.split('/');
+  let navStyle;
+  if((splitPath.includes("shows") || splitPath.includes("reviews")) && splitPath.length===3){
+    navStyle={position:"absolute", zIndex: 3}
+  }
+
+
   return (
-    <div
-      className="nav-bar"
-      style={{
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-        borderBottom: "solid rgb(175, 175, 175)",
-      }}
-    >
+    <div className="nav-bar" style={navStyle}>
       <NavLink className="logo-link" exact to="/shows">
         <img src={logo} alt={logo} />
         <p style={{ margin: "15px 0 0 10px", fontFamily: "'Josefin Sans', sans-serif", fontWeight:700}}>Show 'N Tell</p>
       </NavLink>
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ display: "flex", alignItems: "center" , marginRight: "35px"}}>
         <NavLink className="all-shows-button" exact to="/shows">
           SHOWS
         </NavLink>
