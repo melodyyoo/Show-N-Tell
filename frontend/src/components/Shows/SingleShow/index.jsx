424afe711b304d55a5e3e3333cae6dbb459c5346
Melody Yoo <melodyyoo72@gmail.com>
2023-07-20 08:34:27 -0700

    fix: fix bug where session user's review wouldn't appear in edit review modal on single show page

--- frontend/src/components/Shows/SingleShow/index.jsx
+++ frontend/src/components/Shows/SingleShow/index.jsx
@@ -1,31 +1,41 @@
-import {  useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./SingleShow.css";
 import AllReviews from "../../Reviews/AllReviews";
 import SingleShowLayout from "../../SingleShowLayout";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { thunkGetReview } from "../../../store/reviews";
 
 export default function SingleShow() {
   const show = useSelector((state) => state.shows.show);
   const { showId } = useParams();
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
 
+  useEffect(() => {
+    const userReview = show?.Reviews?.find((review) => review?.userId === sessionUser?.id);
+    if (userReview) {
+      dispatch(thunkGetReview(userReview.id));
+    }
+  });
   if (!show) return null;
 
-  let ongoingShow;
-    if (!show.Show?.endYear) {
-      ongoingShow = <p className="show-year">{show?.Show?.startYear}-</p>;
-    } else if (show?.Show?.startYear === show?.Show?.endYear) {
-      ongoingShow = <p className="show-year">{show?.Show?.startYear}</p>;
-    } else {
-      ongoingShow =  (
-        <p className="show-year">
-          {show?.Show?.startYear}-{show?.Show?.endYear}
-        </p>
-      );
-    }
 
+  let ongoingShow;
+  if (!show.Show?.endYear) {
+    ongoingShow = <p className="show-year">{show?.Show?.startYear}-</p>;
+  } else if (show?.Show?.startYear === show?.Show?.endYear) {
+    ongoingShow = <p className="show-year">{show?.Show?.startYear}</p>;
+  } else {
+    ongoingShow = (
+      <p className="show-year">
+        {show?.Show?.startYear}-{show?.Show?.endYear}
+      </p>
+    );
+  }
 
   return (
-    <SingleShowLayout showId={showId} reviewsOrComments={<AllReviews show={show}/>}>
+    <SingleShowLayout showId={showId} reviewsOrComments={<AllReviews show={show} />}>
       <div className="show-info-wrapper">
         <div className="show-text-wrapper">
           <p className="show-name">{show.Show?.name}</p>
