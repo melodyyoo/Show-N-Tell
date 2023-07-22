ec2dd1f9b5478a1e9bbde1e7d1661e36e1beb6c1
Melody Yoo <melodyyoo72@gmail.com>
2023-07-22 00:39:28 -0700

    feat: add AWS to post and edit a show

---
+++ backend/awsS3.js
@@ -0,0 +1,115 @@
+const AWS = require("aws-sdk");
+// name of your bucket here
+const NAME_OF_BUCKET = "aaprojectsbucket";
+
+const multer = require("multer");
+
+// const upload = multer({
+//     fileFilter: function (req, file, cb){
+//         if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image.jpg'){
+//             cb(null, true);
+//         }else{
+//             cb(new Error('Invalid file type.'))
+//         }
+//     }
+// })
+//  make sure to set environment variables in production for:
+//  AWS_ACCESS_KEY_ID
+//  AWS_SECRET_ACCESS_KEY
+//  and aws will automatically use those environment variables
+
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+// --------------------------- Public UPLOAD ------------------------
+
+const singlePublicFileUpload = async (file) => {
+  const { originalname, mimetype, buffer } = await file;
+  const path = require("path");
+  // name of the file in your S3 bucket will be the date in ms plus the extension name
+  const Key = new Date().getTime().toString() + path.extname(originalname);
+  const uploadParams = {
+    Bucket: NAME_OF_BUCKET,
+    Key,
+    Body: buffer,
+    ACL: "public-read",
+  };
+  const result = await s3.upload(uploadParams).promise();
+
+  // save the name of the file in your bucket as the key in your database to retrieve for later
+  return result.Location;
+};
+
+const multiplePublicFileUpload = async (files) => {
+  return await Promise.all(
+    files.map((file) => {
+      return singlePublicFileUpload(file);
+    })
+  );
+};
+
+// --------------------------- Prviate UPLOAD ------------------------
+
+const singlePrivateFileUpload = async (file) => {
+  const { originalname, mimetype, buffer } = await file;
+  const path = require("path");
+  // name of the file in your S3 bucket will be the date in ms plus the extension name
+  const Key = new Date().getTime().toString() + path.extname(originalname);
+  const uploadParams = {
+    Bucket: NAME_OF_BUCKET,
+    Key,
+    Body: buffer,
+  };
+  const result = await s3.upload(uploadParams).promise();
+
+  // save the name of the file in your bucket as the key in your database to retrieve for later
+  return result.Key;
+};
+
+const multiplePrivateFileUpload = async (files) => {
+  return await Promise.all(
+    files.map((file) => {
+      return singlePrivateFileUpload(file);
+    })
+  );
+};
+
+const retrievePrivateFile = (key) => {
+  let fileUrl;
+  if (key) {
+    fileUrl = s3.getSignedUrl("getObject", {
+      Bucket: NAME_OF_BUCKET,
+      Key: key,
+    });
+  }
+  return fileUrl || key;
+};
+
+// --------------------------- Storage ------------------------
+
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+
+const singleMulterUpload = (nameOfKey) =>
+  multer({ storage: storage }).single(nameOfKey);
+const multipleMulterUpload = (nameOfKey) =>{
+  return multer({ storage: storage }).array(nameOfKey);
+}
+
+// const editMulterUpload = (req, _ , next) =>{
+//     if(req.body.images.length < 2)
+// }
+
+
+module.exports = {
+  s3,
+  singlePublicFileUpload,
+  multiplePublicFileUpload,
+  singlePrivateFileUpload,
+  multiplePrivateFileUpload,
+  retrievePrivateFile,
+  singleMulterUpload,
+  multipleMulterUpload,
+};
--- frontend/src/store/shows.js
+++ frontend/src/store/shows.js
@@ -1,25 +1,22 @@
-import { csrfFetch } from "./csrf"
+import { csrfFetch } from "./csrf";
 
 //TYPES
-const GET_ALL_SHOWS = 'shows/getAllShows'
+const GET_ALL_SHOWS = "shows/getAllShows";
 // const GET_SHOW = "shows/getShow"
-const GET_SHOW_AND_REVIEWS = "shows/getShowsAndReviews"
-const POST_SHOW = 'shows/postShow'
-const EDIT_SHOW = 'shows/editShow'
-const DELETE_SHOW = 'shows/deleteShow'
+const GET_SHOW_AND_REVIEWS = "shows/getShowsAndReviews";
+const POST_SHOW = "shows/postShow";
+const EDIT_SHOW = "shows/editShow";
+const DELETE_SHOW = "shows/deleteShow";
 const DELETE_REVIEW = "reviews/deleteReview";
-
-
-
 
 /*********************************************************************************************************** */
 //ACTION CREATORS
 const actionGetAllShows = (shows) => {
-    return{
-        type: GET_ALL_SHOWS,
-        payload: shows
-    }
-}
+  return {
+    type: GET_ALL_SHOWS,
+    payload: shows,
+  };
+};
 
 // const actionGetShow = (show) =>{
 //     return{
@@ -28,51 +25,51 @@ const actionGetAllShows = (shows) => {
 //     }
 // }
 
-const actionGetShowAndReviews = (show) =>{
-    return{
-        type: GET_SHOW_AND_REVIEWS,
-        payload: show
-    }
-}
+const actionGetShowAndReviews = (show) => {
+  return {
+    type: GET_SHOW_AND_REVIEWS,
+    payload: show,
+  };
+};
 
-const actionPostShow = (show) =>{
-    return{
-        type: POST_SHOW,
-        payload: show
-    }
-}
+const actionPostShow = (show) => {
+  return {
+    type: POST_SHOW,
+    payload: show,
+  };
+};
 
-const actionEditShow = (show) =>{
-    return{
-        type: EDIT_SHOW,
-        payload: show
-    }
-}
+const actionEditShow = (show) => {
+  return {
+    type: EDIT_SHOW,
+    payload: show,
+  };
+};
 
-const actionDeleteShow = (showId) =>{
-    return{
-        type: DELETE_SHOW,
-        payload: showId
-    }
-}
+const actionDeleteShow = (showId) => {
+  return {
+    type: DELETE_SHOW,
+    payload: showId,
+  };
+};
 
 const actionDeleteReview = (reviewId, showId) => {
-    return {
-      type: DELETE_REVIEW,
-      payload:{ reviewId, showId}
-    };
+  return {
+    type: DELETE_REVIEW,
+    payload: { reviewId, showId },
   };
+};
 
 /*********************************************************************************************************** */
 //THUNKS
-export const thunkGetAllShows = () => async(dispatch) =>{
-    const res = await csrfFetch('/api/shows');
+export const thunkGetAllShows = () => async (dispatch) => {
+  const res = await csrfFetch("/api/shows");
 
-    if(res.ok){
-        const data = await res.json();
-        dispatch(actionGetAllShows(data))
-    }
-}
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(actionGetAllShows(data));
+  }
+};
 
 // export const thunkGetShow = (showId) => async(dispatch) =>{
 //     const res = await csrfFetch(`/api/shows/${showId}`);
@@ -83,116 +80,150 @@ export const thunkGetAllShows = () => async(dispatch) =>{
 //     }
 // }
 
-export const thunkGetShowAndReview = (showId) => async(dispatch) =>{
-    const res = await csrfFetch(`/api/shows/${showId}`);
+export const thunkGetShowAndReview = (showId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/shows/${showId}`);
 
-    if(res.ok){
-        const data = await res.json();
-        dispatch(actionGetShowAndReviews(data))
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(actionGetShowAndReviews(data));
+  }
+};
+
+export const thunkPostShow = (show) => async (dispatch) => {
+  const { name, director, synopsis, startYear, endYear, genre, userId, images } = show;
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("director", director);
+  formData.append("synopsis", synopsis);
+  formData.append("startYear", startYear);
+  formData.append("endYear", endYear);
+  formData.append("genre", genre);
+  formData.append("userId", userId);
+
+  if (images && images.length !== 0) {
+    for (var i = 0; i < images.length; i++) {
+      formData.append("images", images[i]);
     }
-}
+  }
 
-export const thunkPostShow = (show) => async(dispatch) =>{
-    const res = await csrfFetch(`/api/shows`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(show)
-    })
+  const res = await csrfFetch(`/api/shows`, {
+    method: "POST",
+    headers: { "Content-Type": "multipart/form-data" },
+    body: formData,
+  });
 
-    if(res.ok){
-        const newShow = await res.json();
-        dispatch(actionPostShow(newShow))
-        return newShow
-    }else{
-        const errors = await res.json();
-        return errors;
+  if (res.ok) {
+    const newShow = await res.json();
+    dispatch(actionPostShow(newShow));
+    return newShow;
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
+
+export const thunkEditShow = (show, showId) => async (dispatch) => {
+  const { name, director, synopsis, startYear, endYear, genre, userId, images } = show;
+  console.log("ENDYEAR IN THUNK: ", endYear);
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("director", director);
+  formData.append("synopsis", synopsis);
+  formData.append("startYear", startYear);
+  if (endYear) formData.append("endYear", endYear);
+  formData.append("genre", genre);
+  formData.append("userId", userId);
+  if (images[0]?.name) formData.append("poster", images[0].name);
+  if (images[1]?.name) formData.append("biggerPoster", images[1].name);
+
+  if (images && images.length !== 0) {
+    for (var i = 0; i < images.length; i++) {
+      formData.append("images", images[i]);
     }
-}
+  }
 
-export const thunkEditShow = (show, showId) => async(dispatch) =>{
-    const res = await csrfFetch(`/api/shows/${showId}`, {
-        method: "PUT",
-        headers:  { "Content-Type": "application/json" },
-        body: JSON.stringify(show)
-    })
+  const res = await csrfFetch(`/api/shows/${showId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "multipart/form-data" },
+    body: formData,
+  });
 
-    if(res.ok){
-        const updatedShow = await res.json();
-        dispatch(actionEditShow(updatedShow))
-        return updatedShow
-    }else{
-        const errors = await res.json();
-        return errors;
-    }
-}
+  if (res.ok) {
+    const updatedShow = await res.json();
+    dispatch(actionEditShow(updatedShow));
+    return updatedShow;
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
 
-export const thunkDeleteShow = (showId) =>async(dispatch)=>{
-    const res = await csrfFetch(`/api/shows/${showId}`, {
-        method: "DELETE"
-    });
+export const thunkDeleteShow = (showId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/shows/${showId}`, {
+    method: "DELETE",
+  });
 
-    if(res.ok){
-        dispatch(actionDeleteShow(showId))
-    }else{
-        const errors = await res.json();
-        return errors;
-    }
-}
+  if (res.ok) {
+    dispatch(actionDeleteShow(showId));
+  } else {
+    const errors = await res.json();
+    return errors;
+  }
+};
 
 export const thunkDeleteReview = (reviewId, showId) => async (dispatch) => {
-    const res = await csrfFetch(`/api/reviews/${reviewId}`, {
-      method: "DELETE",
-    });
+  const res = await csrfFetch(`/api/reviews/${reviewId}`, {
+    method: "DELETE",
+  });
 
-    if (res.ok) {
-      dispatch(actionDeleteReview(reviewId, showId));
-
-    }
-  };
+  if (res.ok) {
+    dispatch(actionDeleteReview(reviewId, showId));
+  }
+};
 
 /*********************************************************************************************************** */
 //REDUCER
-const initialState = {allShows:{}, show:{}}
-const showsReducer = (state=initialState, action) =>{
-    switch(action.type){
-        case GET_ALL_SHOWS:
-            const allShowsState = {...state, allShows: {}};
+const initialState = { allShows: {}, show: {} };
+const showsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ALL_SHOWS:
+      const allShowsState = { ...state, allShows: {} };
 
-            action.payload.forEach((show) =>{
-                allShowsState.allShows[show.id] = show
-            })
-            return allShowsState
+      action.payload.forEach((show) => {
+        allShowsState.allShows[show.id] = show;
+      });
+      return allShowsState;
 
-        // case GET_SHOW:
-        //     const showState = {...state, show:{}};
-        //     showState.show = action.payload;
+    // case GET_SHOW:
+    //     const showState = {...state, show:{}};
+    //     showState.show = action.payload;
 
-        //     return showState;
-        case GET_SHOW_AND_REVIEWS:
-            const showAndReviewState = {...state, show:{}};
-            showAndReviewState.show = action.payload;
-            return showAndReviewState;
-        case POST_SHOW:
-            const newShowState = {allShows:{...state.allShows}, show:action.payload};
-            newShowState.allShows[action.payload.id] = action.payload;
+    //     return showState;
+    case GET_SHOW_AND_REVIEWS:
+      const showAndReviewState = { ...state, show: {} };
+      showAndReviewState.show = action.payload;
+      return showAndReviewState;
+    case POST_SHOW:
+      const newShowState = { allShows: { ...state.allShows }, show: action.payload };
+      newShowState.allShows[action.payload.id] = action.payload;
 
-            return newShowState;
-        case EDIT_SHOW:
-            const updatedShowState = {...state, show:{...state.show}}
-            updatedShowState.show.Show = {...state.show.Show,...action.payload}
-            return updatedShowState;
-        case DELETE_SHOW:
-            const currentAllShows = {...state.allShows};
-            delete currentAllShows[action.payload];
-            return {...state, allShows: currentAllShows}
-        // case DELETE_REVIEW:
-            // const newAllShows = {...state.allShows}
-            // const showId = action.payload.showId
-            // newAllShows[showId].Reviews = newAllShows[showId].Reviews.filter(review=> review.id !== action.payload.reviewId)
-            // return {show:state.show, allShows:newAllShows};
-        default:
-            return state
-    }
-}
+      return newShowState;
+    case EDIT_SHOW:
+      const updatedShowState = { ...state, show: { ...state.show } };
+      updatedShowState.show.Show = { ...state.show.Show, ...action.payload };
+      return updatedShowState;
+    case DELETE_SHOW:
+      const currentAllShows = { ...state.allShows };
+      delete currentAllShows[action.payload];
+      return { ...state, allShows: currentAllShows };
+    // case DELETE_REVIEW:
+    // const newAllShows = {...state.allShows}
+    // const showId = action.payload.showId
+    // newAllShows[showId].Reviews = newAllShows[showId].Reviews.filter(review=> review.id !== action.payload.reviewId)
+    // return {show:state.show, allShows:newAllShows};
+    default:
+      return state;
+  }
+};
 
-export default showsReducer
+export default showsReducer;
