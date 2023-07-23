26621deae0a792427ccaa1aaec98f6b5f7b29352
Melody Yoo <melodyyoo72@gmail.com>
2023-07-22 18:04:06 -0700

    fix: remove useEffect for error messages on LoginFormModal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -3,75 +3,67 @@ import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import "./LoginForm.css";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 
 function LoginFormModal() {
   const dispatch = useDispatch();
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
-  const [validationObject, setValidationObject] = useState({})
   const { closeModal } = useModal();
-  const history = useHistory()
-
-  useEffect(()=>{
-    const errorsObject = {};
-
-    if(credential.length < 4)errorsObject.credential = 'Username must be at least 4 characters';
-
-    if(password.length < 6)errorsObject.password = 'Password must be at least 6 characters';
-
-    setValidationObject(errorsObject);
-  }, [credential, password])
-
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors({});
-    return dispatch(sessionActions.thunkSetSessionUser({ credential, password }))
-      .then(history.push('/shows'))
-      .then(closeModal)
-      .catch(async (res) => {
-        const data = await res.json();
-        if (data && data.errors) {
-          setErrors(data.errors);
-        }
-      });
-  };
 
-  const demoUserLogin = (e) =>{
+    const tempErrors = {};
+    if (credential.length < 4) tempErrors.credential = "Username must be at least 4 characters";
+
+    if (password.length < 6) tempErrors.password = "Password must be at least 6 characters";
+
+    const tempErrorsArray = Object.values(tempErrors);
+    if (tempErrorsArray.length > 0) {
+      setErrors(tempErrors);
+    } else {
+      dispatch(sessionActions.thunkSetSessionUser({ credential, password }))
+        .then(history.push("/shows"))
+        .then(closeModal)
+        .catch(async (res) => {
+          const data = await res.json();
+          if (data && data.errors) {
+            setErrors(data.errors);
+          }
+        });
+    }
+  };
+  const demoUserLogin = (e) => {
     e.preventDefault();
-    return dispatch(sessionActions.thunkSetSessionUser({credential:"Demo-lition",  password: 'password'}))
-    .then(history.push('/shows'))
-    .then(closeModal);
-  }
+    dispatch(sessionActions.thunkSetSessionUser({ credential: "Demo-lition", password: "password" }))
+      .then(history.push("/shows"))
+      .then(closeModal);
+  };
 
   return (
     <div className="login-modal">
       <form onSubmit={handleSubmit}>
-          {errors.credential && (
-            <p className="errors">{errors.credential}</p>
-          )}
+        {errors.credential && <p className="errors">{errors.credential}</p>}
         <label>
           Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
+          <input type="text" value={credential} onChange={(e) => setCredential(e.target.value)} required />
         </label>
         <label>
           Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </label>
-        <button className="log-in-button" disabled={Object.keys(validationObject).length} type="submit">Log In</button>
-        <button className="demo-user-button" onClick={demoUserLogin}>Demo User</button>
+        <div style={{display: "flex", gap:"10px"}}>
+          <button className="log-in-button" type="submit">
+            LOG IN
+          </button>
+          <button className="demo-user-button" onClick={demoUserLogin}>
+            DEMO USER
+          </button>
+        </div>
       </form>
     </div>
   );
