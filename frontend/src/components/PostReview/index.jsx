b473bc18ef7a70274126dfde4b1f1a4550d10c1f
Melody Yoo <melodyyoo72@gmail.com>
2023-07-27 19:20:58 -0700

    style: change font of "review" and "like"

--- frontend/src/components/PostReview/index.jsx
+++ frontend/src/components/PostReview/index.jsx
@@ -12,11 +12,11 @@ export default function PostReview() {
           setModalContent(<AddReviewModal/>)
           setTitle("ADD REVIEW")
           }}className="fa-solid fa-face-smile fa-2xl"></i>
-        <p>Review</p>
+        <p style={{fontFamily: "'Open Sans', sans-serif"}}>Review</p>
       </div>
       <div className="post-review-like" onClick={()=> window.alert("Likes feature coming soon")}>
         <i style={{color: "#EF8733"}}className="fa-solid fa-heart fa-2xl"></i>
-        <p>Like</p>
+        <p style={{fontFamily: "'Open Sans', sans-serif"}}>Like</p>
       </div>
     </div>
   );
