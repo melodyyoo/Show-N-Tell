66efac9288e5782f7d3b1736f0004e4973baf276
Melody Yoo <melodyyoo72@gmail.com>
2023-08-09 17:06:03 -0700

    feat: add CommentMenu component

---
+++ frontend/src/components/Comments/CommentMenu/index.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useRef, useState } from "react";
+import "./CommentMenu.css";
+import OpenModalButton from "../../OpenModalButton";
+import CommentForm from "../CommentForm";
+import DeleteCommentForm from "../DeleteCommentForm";
+
+export default function CommentMenu({ reviewOwner , comment}) {
+  const ulRef = useRef();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  return (
+    <div ref={ulRef} style={{ position: "relative" }}>
+      <p className="comment-menu-button" onClick={openMenu}>
+        ...
+      </p>
+      {showMenu && (
+        <div className="user-info">
+          <OpenModalButton
+            style={{
+              textAlign: "center",
+              color: "white",
+              fontFamily: "'Open Sans', sans-serif",
+              cursor: "pointer",
+              margin: "10px 0",
+              borderBottom: "1px solid gray",
+              paddingBottom: "10px",
+            }}
+            buttonText="Edit Comment"
+            title="EDIT YOUR COMMENT"
+            modalComponent={<CommentForm reviewOwner={reviewOwner} comment={comment}/>}
+          />
+          <OpenModalButton
+            style={{
+              textAlign: "center",
+              color: "white",
+              fontFamily: "'Open Sans', sans-serif",
+              cursor: "pointer",
+              marginBottom: "10px",
+            }}
+            buttonText="Delete Comment"
+            title="PLEASE CONFIRM"
+            modalComponent={<DeleteCommentForm />}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
