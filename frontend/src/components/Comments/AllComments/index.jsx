34bec4f877ed33d85912efdd8da73983f94ac684
Melody Yoo <melodyyoo72@gmail.com>
2023-08-11 08:56:06 -0700

    feat: add comments check in AllComments

--- frontend/src/components/Comments/AllComments/index.jsx
+++ frontend/src/components/Comments/AllComments/index.jsx
@@ -2,6 +2,7 @@ import CommentForm from "../CommentForm";
 import SingleComment from "../SingleComment";
 
 export default function AllComments({ comments, reviewOwner, reviewId }) {
+  if(!comments)return null
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <div style={{width:"600px"}}>
@@ -12,7 +13,7 @@ export default function AllComments({ comments, reviewOwner, reviewId }) {
             borderBottom: "#808080 1px solid",
           }}
         >
-          {comments.length} Comments
+          {comments?.length} Comments
         </p>
         {comments.map((comment) => {
           return <SingleComment reviewId={reviewId} reviewOwner={reviewOwner} comment={comment} key={comment.id} />;
