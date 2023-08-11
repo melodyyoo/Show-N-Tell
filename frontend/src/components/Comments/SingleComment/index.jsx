448317f5b72dc3ccebd16c937a46ce78fb6abfbf
Melody Yoo <melodyyoo72@gmail.com>
2023-08-11 08:57:20 -0700

    feat: add comment check in SingleComment

--- frontend/src/components/Comments/SingleComment/index.jsx
+++ frontend/src/components/Comments/SingleComment/index.jsx
@@ -4,6 +4,7 @@ import CommentMenu from "../CommentMenu";
 
 export default function SingleComment({ comment , reviewOwner, reviewId}) {
   const sessionUser = useSelector((state) => state.session.user);
+  if(!comment)return null;
 
   function convertDate() {
     return new Date(comment.createdAt).toLocaleDateString("en-US", {
@@ -14,10 +15,10 @@ export default function SingleComment({ comment , reviewOwner, reviewId}) {
   }
   return (
     <div>
-      {sessionUser?.username === comment?.User?.username && <CommentMenu comment={comment} reviewOwner={reviewOwner} reviewId={reviewId}/>}
+      {sessionUser?.username === comment.User?.username && <CommentMenu comment={comment} reviewOwner={reviewOwner} reviewId={reviewId}/>}
       <div className="comment-box">
         <div>
-          <p className="user-date-comment" >{comment?.User?.username}</p>
+          <p className="user-date-comment" >{comment.User?.username}</p>
           <p className="user-date-comment">{convertDate()}</p>
         </div>
         <div style={{ width: "350px" }}>{comment.body}</div>
