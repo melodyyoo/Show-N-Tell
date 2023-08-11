066a4a7796befe487eb904224ccb1646d936eecf
Melody Yoo <melodyyoo72@gmail.com>
2023-08-10 17:46:35 -0700

    feat: add commentsRouter to api/index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const showsRouter = require('./shows.js');
 const reviewsRouter = require('./reviews.js');
+const commentsRouter = require('./comments.js')
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -15,6 +16,8 @@ router.use('/users', usersRouter);
 router.use('/shows', showsRouter);
 
 router.use('/reviews', reviewsRouter);
+
+router.use('/comments', commentsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
